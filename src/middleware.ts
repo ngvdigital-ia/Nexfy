6fc29a1447bd7470a8b0800f1b0ae6a995c15cd5
PedroVictor26 <pedrovictorsilvaoliveira10@gmail.com>
2026-01-30 11:30:42 -0300

    fix: permitir .webp no middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -112,6 +112,6 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.svg$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.svg$|.*\\.webp$).*)",
   ],
 };
